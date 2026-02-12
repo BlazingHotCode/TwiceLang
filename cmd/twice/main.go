0762a8401640ce7c5fdb7c6a845fbc9856394d7b
BlazingHotCode <nadav.babad@gmail.com>
2026-02-12 17:16:46 +0200

    added basic tokenization to main

--- cmd/twice/main.go
+++ cmd/twice/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"twice/internal/lexer"
+	"twice/internal/token"
+)
 
 func main() {
-	fmt.Println("twice: hello, compiler!")
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+	x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
+`
+
+	l := lexer.New(input)
+
+	// Keep getting tokens until we hit EOF
+	for {
+		tok := l.NextToken()
+		fmt.Printf("%+v\n", tok)
+		
+		if tok.Type == token.EOF {
+			break
+		}
+	}
 }
